0e3eb3792df00060e9eec4798293d11004ce1e54
korashy2018 <a.ashraf.elgammal@hotmail.com>
2023-08-09 23:15:20 +0000

    add the skeleton of stack

---
+++ Stack/Stack.js
@@ -0,0 +1,16 @@
+const LinkedList = require('../LinkedList/LinkedList.js');
+module.exports = class Stack {
+  data;
+  constructor(){
+    
+  }
+  push(_data){
+    
+  }
+  pop(){
+    
+  }
+  peek(){
+    
+  }
+}
