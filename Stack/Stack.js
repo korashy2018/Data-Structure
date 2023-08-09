8dd2011a985d1a81b8f234ae5dc2e1031ec59296
korashy2018 <a.ashraf.elgammal@hotmail.com>
2023-08-09 23:59:03 +0000

    finish up stack implementation

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,16 +1,31 @@
 const LinkedList = require('../LinkedList/LinkedList.js');
 module.exports = class Stack {
-  data;
-  constructor(){
-    
+  #dataList;
+  constructor(unique) {
+    this.#dataList = new LinkedList(unique ?? false);
   }
-  push(_data){
-    
+  push(_data) {
+    if (this.#dataList.alreadyExistNode(_data)) return;
+    this.#dataList.insertFirst(_data);
   }
-  pop(){
-    
+  pop() {
+    var head = this.#dataList.head.data;
+    this.#dataList.deleteHead();
+    return head;
+
   }
-  peek(){
-    
+  peek() {
+    return this.#dataList.head.data;
+
+  }
+  size() {
+    return this.#dataList.length;
+  }
+
+  isEmpty() {
+    return this.#dataList.head == null;
+  }
+  print() {
+    this.#dataList.printList();
   }
 }
